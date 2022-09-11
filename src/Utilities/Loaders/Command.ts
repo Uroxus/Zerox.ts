4871e9aa044e9de7b253bfce4430ac72140f7fed
Uroxus <thepointlessemails@gmail.com>
2022-09-11 16:35:54 +0100

    Update file filtering

--- src/Utilities/Loaders/Command.ts
+++ src/Utilities/Loaders/Command.ts
@@ -3,15 +3,17 @@ import { CommandMap } from "../../Constants/Maps/Commands.js";
 import type { Command } from "../../Classes/Command.js";
 
 export async function loadCommands () {
-    for await ( const file of fetchFiles( "./Events/" ) ) {
-        const { "default": CommandFile } = await import( file );
+    for await ( const file of fetchFiles( "./Commands/" ) ) {
+        if ( file.includes( ".handler" ) ) {
+            const { "default": CommandFile } = await import( file );
 
-        const commandInstance: Command = new CommandFile();
+            const commandInstance: Command = new CommandFile();
 
-        if ( !CommandMap.has( commandInstance.commandName ) ) {
-            CommandMap.set( commandInstance.commandName, commandInstance );
-        } else {
-            console.error( `Attempted to load duplicate command ${ commandInstance.commandName } - no changes were made` );
+            if ( !CommandMap.has( commandInstance.commandName ) ) {
+                CommandMap.set( commandInstance.commandName, commandInstance );
+            } else {
+                console.error( `Attempted to load duplicate command ${ commandInstance.commandName } - no changes were made` );
+            }
         }
     }
 }
