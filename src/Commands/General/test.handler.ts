e265db67ab5fd9b513c6a70a2fe22ba2c9b00f8b
Uroxus <thepointlessemails@gmail.com>
2022-09-15 19:29:29 +0100

    Implement component in command replies

--- src/Commands/General/test.handler.ts
+++ src/Commands/General/test.handler.ts
@@ -1,5 +1,6 @@
 import { Command } from "../../Classes/Command.js";
-import type { Message, AnyTextChannel, CommandInteraction } from "oceanic.js";
+import { Message, AnyTextChannel, CommandInteraction, ComponentTypes } from "oceanic.js";
+import DeleteParent from "../../Components/DeleteParent.js";
 
 export default class Test extends Command {
     constructor() {
@@ -7,12 +8,30 @@ export default class Test extends Command {
     }
 
     public textCommand ( Message: Message<AnyTextChannel> ): void {
-        Message.channel.createMessage( { "messageReference": { "messageID": Message.id }, "content": "Test text command reply" } ).catch( err => {
+        Message.channel.createMessage( {
+            "messageReference": { "messageID": Message.id }, "content": "Test text command reply", "components": [
+                {
+                    "type": ComponentTypes.ACTION_ROW,
+                    "components": [
+                        DeleteParent.Definition
+                    ]
+                }
+            ]
+        } ).catch( err => {
             console.error( `Failed to respond to Test textCommand: ${ err }` );
         } );
     }
 
     public slashCommand ( Interaction: CommandInteraction ): void | Promise<void> {
-        Interaction.createMessage( { "content": "Hello from Slash command" } );
+        Interaction.createMessage( {
+            "content": "Hello from Slash command", "components": [
+                {
+                    "type": ComponentTypes.ACTION_ROW,
+                    "components": [
+                        DeleteParent.Definition
+                    ]
+                }
+            ]
+        } );
     }
 }
