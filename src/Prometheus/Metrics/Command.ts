5c375c9c2df4251524d43f5a30a0ae0fbff7f208
Uroxus <thepointlessemails@gmail.com>
2022-09-26 18:46:38 +0100

    Implement interaction/component counter

--- src/Prometheus/Metrics/Command.ts
+++ src/Prometheus/Metrics/Command.ts
@@ -3,7 +3,21 @@ import { Counter } from 'prom-client';
 
 export const textCommandCount = new Counter( {
     "name": `${ AppName }_textCommandCount`,
-    "help": "The number of times each text command has been used",
+    "help": "The number of times a text command has been used",
     "labelNames": [ "command" ] as const,
+    "registers": [ Register ]
+} );
+
+export const interactionCommandCount = new Counter( {
+    "name": `${ AppName }_interactionCommandCount`,
+    "help": "The number of times an interaction command has been used",
+    "labelNames": [ "type", "command" ] as const,
+    "registers": [ Register ]
+} );
+
+export const componentCommandCount = new Counter( {
+    "name": `${ AppName }_componentCommandCount`,
+    "help": "The number of times a component has been used",
+    "labelNames": [ "name" ] as const,
     "registers": [ Register ]
 } );
